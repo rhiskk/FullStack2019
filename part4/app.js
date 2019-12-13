034a9db2e3afc6ee402691a4cebd05f282d229de
hiski <hiski.r@gmail.com>
2019-12-13 15:38:46 +0200

    update

--- part4/app.js
+++ part4/app.js
@@ -5,8 +5,8 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require('cors')
 const middleware = require('./utils/middleware')
-const blogsRouter = require("./controllers/blogs");
-const usersRouter = require("./controllers/users")
+const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
@@ -15,9 +15,23 @@ app.use(cors())
 app.use(express.static("build"))
 app.use(bodyParser.json());
 app.use(middleware.tokenExtractor)
-app.use("/api/blogs", blogsRouter);
+app.use('/api/blogs', blogsRouter);
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
+  .then(() => {
+      console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+      console.log('error connection to MongoDB:', error.message)
+  })
+
+if (process.env.NODE_ENV === 'test') {
+    const testingRouter = require('./controllers/testing')
+    app.use('/api/testing', testingRouter)
+    console.log('running in test mode')
+  }
 
 app.use(middleware.errorHandler)
 
