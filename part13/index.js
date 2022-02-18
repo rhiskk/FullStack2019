09d2b9b0e5da1255e367b82b93934791b91e7e65
hiski <hiski.r@gmail.com>
2022-02-18 09:54:32 +0200

    13.4

--- part13/index.js
+++ part13/index.js
@@ -1,27 +1,73 @@
-require('dotenv').config()
-const { Sequelize, QueryTypes } = require('sequelize')
+require("dotenv").config();
+const { Sequelize, Model, DataTypes } = require("sequelize");
+const express = require("express");
+const app = express();
+app.use(express.json());
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false
-    }
+      rejectUnauthorized: false,
+    },
   },
-})
+});
 
-const main = async () => {
+class Blog extends Model {}
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    author: {
+      type: DataTypes.TEXT,
+    },
+    url: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    likes: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: "blog",
+  },
+);
+
+app.get("/api/blogs", async (req, res) => {
+  const blogs = await Blog.findAll();
+  res.json(blogs);
+});
+
+app.post("/api/blogs", async (req, res) => {
+  console.log(req.body);
   try {
-    await sequelize.authenticate()
-    const blogs = await sequelize.query("SELECT * FROM blogs", { type: QueryTypes.SELECT })
-    for (let i = 0; i < blogs.length; i++) {
-        const blog = blogs[i];
-        console.log(`${blog.author}: '${blog.title}', ${blog.likes} likes`)
-    }
-    sequelize.close()
+    const blog = await Blog.create(req.body);
+    res.json(blog);
   } catch (error) {
-    console.error('Unable to connect to the database:', error)
+    return res.status(400).json({ error });
   }
-}
+});
 
-main()
+app.delete("/api/blogs/:id", async (req, res) => {
+  await Blog.destroy({
+    where: { id: req.params.id },
+  });
+  res.status(204).end();
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
